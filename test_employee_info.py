fa489cb545be03dd7bbcdf439e3876378b0ac7b0
department-of-observation <113044066+department-of-observation@users.noreply.github.com>
2023-05-04 12:54:15 +0800

    implemented test cases v4

---
+++ test_employee_info.py
@@ -0,0 +1,13 @@
+import employee_info
+def test_get_employees_by_age_range():
+ result = 0
+ input=30
+ result=employee_info.calculate_bmi(1.6, input)
+ test =-1
+ assert (result == test)
+
+def test_calculate_average_salary():
+    result = employee_info.
+
+def test_get_employees_by_dept():
+    result = employee_info.
